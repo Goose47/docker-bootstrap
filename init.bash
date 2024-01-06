cefd746861c194bac632013f7d6d9ac4bee337d0
Nikita <tyumin000@gmail.com>
2024-01-06 15:10:22 +0300

    fixed script

--- init.bash
+++ init.bash
@@ -8,4 +8,4 @@ bash ./commands/install_docker.bash
 # install docker compose
 bash ./commands/install_docker_compose.bash
 # add current user to docker group
-bash ./commands/add_user_to_docker_group.bash username
+bash ./commands/add_user_to_docker_group.bash $username
