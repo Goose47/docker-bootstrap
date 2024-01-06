0b63d21b3732e1269723095e90d9380636dd886e
Nikita <tyumin000@gmail.com>
2024-01-06 15:38:41 +0300

    added confirmations and cleaned up

--- init.bash
+++ init.bash
@@ -1,11 +1,61 @@
 #!/bin/bash
 
-# Get the username of current user
-username=$USER
+
+# HELPER FUNCTIONS
+prompt_user() {
+    local message=$1
+    # Ask for confirmation
+    read -p "$message" choice
+    case "$choice" in
+          [yY] | [yY][eE][sS])
+            # operation confirmed
+            return 0
+            ;;
+          [nN] | [nN][oO])
+            # operation canceled
+            return 1
+            ;;
+          *)
+            # invalid choice
+            return 2
+            ;;
+    esac
+}
+
+# MAIN SCRIPT
 
 # install docker
-bash ./commands/install_docker.bash
+while true; do
+    prompt_user "Do you want to install docker? (Y/n)  "
+    case $? in
+        0)
+          bash ./commands/install_docker.bash
+          break ;;
+        1) break ;;
+        *) continue ;;
+    esac
+done
+
 # install docker compose
-bash ./commands/install_docker_compose.bash
+while true; do
+    prompt_user "Do you want to install install docker compose? (Y/n)  "
+    case $? in
+        0)
+          bash ./commands/install_docker_compose.bash
+          break ;;
+        1) break ;;
+        *) continue ;;
+    esac
+done
+
 # add current user to docker group
-bash ./commands/add_user_to_docker_group.bash $username
+while true; do
+    prompt_user "Do you want to add current user to docker group (for running docker commands without sudo)? (Y/n)  "
+    case $? in
+        0)
+          bash ./commands/add_user_to_docker_group.bash $USER
+          break ;;
+        1) break ;;
+        *) continue ;;
+    esac
+done
